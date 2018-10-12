c94dc9282be2ddede6e7a20f6c239b32c182b4fd
jayagl <jagrawal@isw.net.au>
2018-10-12 16:24:53 +1000

    Interactive feedback on /anonymize
    - confirm alias on first run and on `change-alias`
    - move response formatting logic from createTargetedMessage() into format()
    - rename pseudonym to alias

--- src/anonymize.js
+++ src/anonymize.js
@@ -7,19 +7,53 @@ const chance = new Chance();
 const aliases = {};
 
 export default function postAnonymously({ text, userId, spaceId, token }) {
-  //generate alias for user
-  if (!aliases[userId]) aliases[userId] = chance.last();
+  //if text starts with change-alias, set the change flag
+  const change = text.match(/^\s*change-alias(.*)/);
+  if (change) text = change[1].trim();
+
+  let alias = aliases[userId];
+
+  //need to confirm alias on first run or if running change-alias
+  const needToConfirm = !alias || change;
+
+  //send confirmation response if alias confirmation is required
+  if (needToConfirm) {
+    aliases[userId] = alias = chance.last();
+
+    return Promise.resolve({
+      title: 'Please Confirm Alias',
+      text: `Your messages will be posted under the alias "${alias}"`,
+      //button payload needs to have full context of command, so we pass in the text as well
+      buttons: [
+        {
+          text: 'Confirm',
+          payload: `/anonymize ${text}`,
+          style: 'PRIMARY',
+        },
+        {
+          text: 'Shuffle',
+          payload: `/anonymize change-alias ${text}`,
+          style: 'SECONDARY',
+        },
+      ],
+    });
+  }
+
+  //if it was just a change-alias request
+  if (!text) return Promise.resolve({ text: 'Your request was processed' });
+
   //post message
   return sendMessage(
     {
       spaceId,
       text,
-      actorName: aliases[userId],
-      title: '(Pseudonym)',
+      actorName: alias,
+      title: '(Alias)',
     },
     token
   ).then(() => ({
     title: 'Your message was sent!',
-    text: `Your message was posted under the pseudonym *${aliases[userId]}*`,
+    text: `Your message was posted under the alias *${aliases[userId]}*
+    Run the command \`/anonymize change-alias\` to change your alias.`,
   }));
 }
--- src/targeted.js
+++ src/targeted.js
@@ -1,19 +1,8 @@
-import { jsonToGraphQLQuery } from 'json-to-graphql-query';
+import { EnumType, jsonToGraphQLQuery } from 'json-to-graphql-query';
 import { pickBy } from 'lodash';
 import { executeGraphql } from './utils';
 
 export default function createTargetedMessage(response, annotation, token) {
-  const { title, text } = response;
-
-  //default message
-  if (!text && !title)
-    response = {
-      title: 'Error',
-      text: 'Oops something went wrong. Please try again.',
-    };
-  //remove any null or unexpected properties
-  else response = pickBy({ title, text }, prop => !!prop);
-
   //json query is easier to construct
   const { conversationId, createdBy, targetDialogId } = annotation;
   if (!targetDialogId) return Promise.reject('No targetDialogId in annotation');
@@ -28,7 +17,7 @@ export default function createTargetedMessage(response, annotation, token) {
             targetDialogId,
             annotations: [
               {
-                genericAnnotation: response,
+                genericAnnotation: format(response),
               },
             ],
           },
@@ -40,4 +29,33 @@ export default function createTargetedMessage(response, annotation, token) {
 
   //convert json to graphql query
   return executeGraphql(jsonToGraphQLQuery(query), token);
+}
+
+//translate response to WW API requirements
+function format(response = {}) {
+  const { title, text } = response;
+  let { buttons } = response;
+
+  //default message
+  if (!text && !title)
+    response = {
+      title: 'Error',
+      text: 'Oops something went wrong. Please try again.',
+    };
+
+  //adjust buttons syntax
+  if (buttons) {
+    buttons = buttons.map(({ text, payload, style }) => ({
+      postbackButton: {
+        title: text,
+        id: payload,
+        style: new EnumType(style),
+      },
+    }));
+  }
+
+  //remove any null or unexpected properties
+  response = pickBy({ title, text, buttons }, prop => !!prop);
+
+  return response;
 }
