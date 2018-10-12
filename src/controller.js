bf2e90752ababa37e199a55bdbbb7f6440c701ac
jayagl <jagrawal@isw.net.au>
2018-10-12 19:38:51 +1000

    Split controller and webhook-controller

--- src/controller.js
+++ src/controller.js
@@ -4,9 +4,7 @@ import getProfile from './profile';
 import getSpaces from './spaces';
 import sendMessage from './message';
 import { handleWebhook, getWebhookHistory } from './webhook';
-import postAnonymously from './anonymize';
-import postRelevantXkcd from './xkcd';
-import createTargetedMessage from './targeted';
+import execute from './webhook-controller';
 
 const { appId, secret, redirectUri } = config;
 
@@ -70,118 +68,10 @@ export function authorize(req, res) {
 
 //webhook handler
 export function webhook(req, res) {
-  handleWebhook(req, res, config, execute);
+  handleWebhook(req, res, config, execute.bind(null, appAuth.access_token));
 }
 
 //list received webhooks
 export function webhooks(req, res) {
   res.render('webhooks', { webhooks: getWebhookHistory() });
-}
-
-//handler for webhook events
-function execute(event) {
-  const { actionId, spaceId, userId, userName, annotationType } = event;
-  const token = appAuth.access_token;
-  switch (annotationType) {
-    case 'welcome': {
-      sendMessage(
-        {
-          spaceId,
-          title: 'Howdy! 🎉',
-          text: `Thanks for adding me! 🙏
-          [Here](https://www.google.com) is some important info about how to make the most of my abilities.
-          I look forward to helping you! 😀`,
-        },
-        token
-      );
-      break;
-    }
-
-    case 'actionSelected': {
-      const [action, ...params] = actionId.split(/\s/);
-      const text = params.join(' ');
-      let executing;
-      if (action === '/anonymize') {
-        executing = postAnonymously({
-          text,
-          userId,
-          spaceId,
-          token: appAuth.access_token,
-        });
-      } else if (action === '/xkcd') {
-        executing = postRelevantXkcd({
-          search: text,
-          userId,
-          spaceId,
-          userName,
-          token,
-        });
-      }
-
-      //send feedback if executing action
-      if (executing)
-        executing
-          .catch(err => console.log('Error executing', err) || err)
-          .then(response => createTargetedMessage(response, event, token))
-          .catch(err => console.log('Error creating targeted dialog', err));
-      break;
-    }
-
-    //Focus annotation
-    case 'message-focus':
-      console.log(
-        '\nWatson has identified a Focus - lens, confidence, phrase, categories\n',
-        event.lens, // 'ActionRequest', 'Question' 'Commitment'
-        event.confidence,
-        event.phrase,
-        event.categories
-      );
-      break;
-
-    //Moment annotation
-    case 'conversation-moment':
-      console.log(
-        '\nWatson identified a moment - participants, phrases, mostRelevantMessage\n',
-        event.participants,
-        event.momentSummary.phrases,
-        event.momentSummary.mostRelevantMessage
-      );
-      break;
-
-    //Information extraction annotation
-    case 'message-nlp-docSentiment':
-      console.log(
-        '\nWatson analyzed message sentiment\n',
-        event.docSentiment.type,
-        event.docSentiment.score
-      );
-      break;
-    case 'message-nlp-keywords':
-      console.log(
-        '\nWatson identified some keywords\n',
-        event.keywords //text, relevance
-      );
-      break;
-    case 'message-nlp-concepts':
-      console.log(
-        '\nWatson identified some concepts\n',
-        event.concepts //dbpedia, text, relevance
-      );
-      break;
-    case 'message-nlp-entities':
-      console.log(
-        '\nWatson identified entities\n',
-        event.entities //text, type, relevance
-      );
-      break;
-    case 'message-nlp-taxonomy':
-      console.log(
-        '\nWatson has identified taxonomy\n',
-        event.taxonomy //label, score, confident
-      );
-      break;
-
-    default:
-      console.log('Unactionable annotationType', annotationType);
-  }
 }
---
+++ src/webhook-controller.js
@@ -0,0 +1,111 @@
+import sendMessage from './message';
+import postAnonymously from './anonymize';
+import postRelevantXkcd from './xkcd';
+import createTargetedMessage from './targeted';
+
+//handler for webhook events
+export default function execute(token, event) {
+  const { actionId, spaceId, userId, userName, annotationType } = event;
+  switch (annotationType) {
+    case 'welcome': {
+      sendMessage(
+        {
+          spaceId,
+          title: 'Howdy! 🎉',
+          text: `Thanks for adding me! 🙏
+          [Here](https://www.google.com) is some important info about how to make the most of my abilities.
+          I look forward to helping you! 😀`,
+        },
+        token
+      );
+      break;
+    }
+
+    case 'actionSelected': {
+      const [action, ...params] = actionId.split(/\s/);
+      const text = params.join(' ');
+      let executing;
+      if (action === '/anonymize') {
+        executing = postAnonymously({
+          text,
+          userId,
+          spaceId,
+          token,
+        });
+      } else if (action === '/xkcd') {
+        executing = postRelevantXkcd({
+          search: text,
+          userId,
+          spaceId,
+          userName,
+          token,
+        });
+      }
+
+      //send feedback if executing action
+      if (executing)
+        executing
+          .catch(err => console.log('Error executing', err) || err)
+          .then(response => createTargetedMessage(response, event, token))
+          .catch(err => console.log('Error creating targeted dialog', err));
+      break;
+    }
+
+    //Focus annotation
+    case 'message-focus':
+      console.log(
+        '\nWatson has identified a Focus - lens, confidence, phrase, categories\n',
+        event.lens, // 'ActionRequest', 'Question' 'Commitment'
+        event.confidence,
+        event.phrase,
+        event.categories
+      );
+      break;
+
+    //Moment annotation
+    case 'conversation-moment':
+      console.log(
+        '\nWatson identified a moment - participants, phrases, mostRelevantMessage\n',
+        event.participants,
+        event.momentSummary.phrases,
+        event.momentSummary.mostRelevantMessage
+      );
+      break;
+
+    //Information extraction annotation
+    case 'message-nlp-docSentiment':
+      console.log(
+        '\nWatson analyzed message sentiment\n',
+        event.docSentiment.type,
+        event.docSentiment.score
+      );
+      break;
+    case 'message-nlp-keywords':
+      console.log(
+        '\nWatson identified some keywords\n',
+        event.keywords //text, relevance
+      );
+      break;
+    case 'message-nlp-concepts':
+      console.log(
+        '\nWatson identified some concepts\n',
+        event.concepts //dbpedia, text, relevance
+      );
+      break;
+    case 'message-nlp-entities':
+      console.log(
+        '\nWatson identified entities\n',
+        event.entities //text, type, relevance
+      );
+      break;
+    case 'message-nlp-taxonomy':
+      console.log(
+        '\nWatson has identified taxonomy\n',
+        event.taxonomy //label, score, confident
+      );
+      break;
+
+    default:
+      console.log('Unactionable annotationType', annotationType);
+  }
+}
